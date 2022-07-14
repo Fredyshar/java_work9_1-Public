2a6960f97eb3de9d425549b617b3a1569e4132f0
Fedor Sharov <1710193@mail.ru>
2022-07-14 07:36:35 +0300

    first

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,6 @@
+public class FormDate {
+
+    public int day; // поле для дня рожддения
+    public int month; // поле для месяца рожддения
+    public int year; // поле для года рожддения
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,17 @@
+public class Post {
+    public FormDate birthday; // поле для даты рождения
+    public String name; // поле для имени
+
+    public String passport; // поле для номера паспорта
+
+    public String patronymic; // поле для отчества
+
+    public String phone; // поле для номера телефона
+
+    public String surname; // поле для фамилии
+
+    public boolean subscription; // поле для информации о регистрации
+}
+
+
+
